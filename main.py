85cb0a76460f2064dff9bc995de853fffeed89ca
Алеша <98235139+Kolinotbityi@users.noreply.github.com>
2024-09-08 12:54:42 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print(12+89-1) #пример
+
+#1st program
+print('#1st program - float')
+print((9 ** 0.5) * 5)
+
+#2st program
+print('#2st programv - bool')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+print('#3st program')
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a == b)
+
+#4st program
+print('#4st program')
+a = '123.456'
+print(a)
+print(type(float(a)))
+b = (float(a) * 10)
+print(int(b) % 10)
+
